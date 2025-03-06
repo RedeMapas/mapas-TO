38146c38eeec3bab1fda5804773c54868ac83c15
Victor Ferreira <vitfera@hotmail.com>
2025-03-06 17:18:57 -0300

    instalação de plugins necessários

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -4,5 +4,9 @@ return [
     'plugins' => [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'Accessibility' => ['namespace' => 'Accessibility'],
+        'AdminLoginAsUser' => ['namespace' => 'AdminLoginAsUser'],
+        'Analytics' => ['namespace' => 'Analytics'],
+        'MapasBlame' => ['namespace' => 'MapasBlame'],
     ]
 ];
